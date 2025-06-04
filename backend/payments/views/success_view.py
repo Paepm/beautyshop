1b0442aab429b0262e60fabeeead418a8b239116
Patrick <patrick.mair@sodex.at>
2025-06-04 20:56:06 +0200

    integrated ci.yml

--- backend/payments/views/success_view.py
+++ backend/payments/views/success_view.py
@@ -1,10 +1,14 @@
 # payments/views/success_view.py
-from django.shortcuts import redirect
-from django.views import View
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from devtools import debug
+from rest_framework.permissions import IsAuthenticated
 
 
-class PaymentSuccessView(View):
+class PaymentSuccessView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, order_id):
         debug(f"[SUCCESS_VIEW] Payment successful for order #{order_id}")
-        return redirect(f"http://localhost:3000/payments/success_payment/{order_id}/")
+        return Response(f"http://localhost:3000/payments/success_payment/{order_id}/")
