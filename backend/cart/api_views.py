2b4dbf316a33890056da68810fddb5c21d7e12f9
Patrick <patrick.mair@sodex.at>
2025-06-04 21:49:52 +0200

    integrated ci.yml

--- backend/cart/api_views.py
+++ backend/cart/api_views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
-from backend.cart.services.cart_services import CartService
+from cart.services.cart_services import CartService
 
 
 class CartDetailView(APIView):
