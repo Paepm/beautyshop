3e6de75a325446154edfeb61c2abd5e99da9a14e
Patrick Mair <mair.patrick@gmx.at>
2025-06-27 12:57:52 +0200

    split dev and prod settings

---
+++ backend/beautyshop/settings/dev.py
@@ -0,0 +1,36 @@
+# backend/beautyshop/settings/dev.py
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "1753-62-178-83-52.ngrok-free.app",  # Dein ngrok dev Host
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "beautyshop",
+        "USER": "beautyshop_user",
+        "PASSWORD": "beautyshop_pass",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
+# CORS und CSRF für localhost:3000 (React dev server)
+CSRF_COOKIE_HTTPONLY = False
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SAMESITE = "Lax"
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
