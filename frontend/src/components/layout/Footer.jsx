df3e2f75392c3c4fc3704102b9bfb6a91d866272
Patrick Mair <mair.patrick@gmx.at>
2025-12-13 13:11:55 +0100

    solved footer flexbox justify content center bug

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -7,6 +7,24 @@ function Footer() {
     return (
         <footer className="bg-yellow-100 border-b border-yellow-300 px-4 py-2">
             <div className="max-w-7xl mx-auto px-4 py-4 text-sm text-gray-600">
+            {/* Linkliste – zentriert */}
+                <div className="flex-1 flex justify-center flex-wrap gap-x-6 gap-y-2 text-center">
+                    <Link to="/contact" className="hover:underline">
+                        Contact Us
+                    </Link>
+                    <Link to="/privacy" className="hover:underline">
+                        Data Protection
+                    </Link>
+                    <Link to="/gtc" className="hover:underline">
+                        GTC
+                    </Link>
+                    <Link to="/imprint" className="hover:underline">
+                        Imprint
+                    </Link>
+                </div>
+            </div>
+
+            <div className="max-w-7xl mx-auto px-4 py-4 text-sm text-gray-600">
 
                 {/* Haupt-Layout: 3-Spalten-Ansatz */}
                 <div className="flex flex-wrap justify-between items-start gap-y-4">
@@ -20,22 +38,6 @@ function Footer() {
                             <img src="/media/shop_page/visa_logo.png" alt="Visa" className="h-5" />
                             <img src="/media/shop_page/klarna_logo.png" alt="Klarna" className="h-6" />
                         </div>
-                    </div>
-
-                    {/* Linkliste – zentriert */}
-                    <div className="flex-1 flex justify-center flex-wrap gap-x-6 gap-y-2 text-center">
-                        <Link to="/contact" className="hover:underline">
-                            Contact Us
-                        </Link>
-                        <Link to="/privacy" className="hover:underline">
-                            Data Protection
-                        </Link>
-                        <Link to="/gtc" className="hover:underline">
-                            GTC
-                        </Link>
-                        <Link to="/imprint" className="hover:underline">
-                            Imprint
-                        </Link>
                     </div>
 
                     {/* Sprachwahl – rechts */}
